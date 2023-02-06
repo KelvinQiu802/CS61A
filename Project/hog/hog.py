8d62cce4cc6b75fe3e2536196fade687280e2eb9
KelvinQiu <kelvinqiu802@outlook.com>
2023-02-06 17:36:47 +0800

    feat: problem 6

--- Project/hog/hog.py
+++ Project/hog/hog.py
@@ -137,6 +137,7 @@ def play(strategy0, strategy1, score0=0, score1=0, dice=six_sided,
         else:
             score1, score0, p1_last_pure_score = calc_score(
                 strategy1, score1, score0, p1_last_pure_score)
+        say = say(score0, score1)
         who = other(who)
     # END PROBLEM 5
     # (note that the indentation for the problem 6 prompt (***YOUR CODE HERE***) might be misleading)
