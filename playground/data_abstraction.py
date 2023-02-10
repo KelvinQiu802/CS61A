68055ee8b3a477aa94357be3d6e7ea915239cce3
KelvinQiu <kelvinqiu802@outlook.com>
2023-02-10 16:24:40 +0800

    refactor: function impl

--- playground/data_abstraction.py
+++ playground/data_abstraction.py
@@ -6,10 +6,13 @@ def add_rational(r1, r2):
 
     >>> r1 = rational(1, 2)
     >>> r2 = rational(2, 3)
-    >>> add_rational(r1, r2)
-    [7, 6]
+    >>> result = add_rational(r1, r2)
+    >>> print_rational(result)
+    7 / 6
     """
-    return rational(numer(r1) * denom(r2) + numer(r2) * denom(r1), denom(r1) * denom(r2))
+    n_r1, d_r1 = numer(r1), denom(r1)
+    n_r2, d_r2 = denom(r1), denom(r2)
+    return rational(n_r1 * d_r2 + n_r2 * d_r1, d_r1 * d_r2)
 
 
 def mul_rational(r1, r2):
@@ -17,22 +20,49 @@ def mul_rational(r1, r2):
 
     >>> r1 = rational(1, 2)
     >>> r2 = rational(2, 3)
-    >>> mul_rational(r1, r2)
-    [1, 3]
+    >>> result = mul_rational(r1, r2)
+    >>> print_rational(result)
+    1 / 3
     """
     return rational(numer(r1) * numer(r2), denom(r1) * denom(r2))
+
+
+def calc_rational(r):
+    """Calculate the rational to float.
+
+    >>> calc_rational(rational(1, 2))
+    0.5
+    >>> calc_rational(rational(2, 1))
+    2.0
+    """
+    return numer(r) / denom(r)
+
+
+def print_rational(r):
+    """Print the rational.
+
+    >>> print_rational(rational(1, 2))
+    1 / 2
+    """
+    print(numer(r), '/', denom(r))
 
 
 def rational(n, d):
     """Construct a rational number x that represents n/d.
 
-    >>> rational(2, 3)
-    [2, 3]
-    >>> rational(5, 10)
-    [1, 2]
+    >>> print_rational(rational(2, 3))
+    2 / 3
+    >>> print_rational(rational(5, 10))
+    1 / 2
     """
     g = gcd(n, d)
-    return [n // g, d // g]
+
+    def selector(name):
+        if name == 'n':
+            return n // g
+        elif name == 'd':
+            return d // g
+    return selector
 
 
 def numer(r):
@@ -41,7 +71,7 @@ def numer(r):
     >>> numer(rational(2, 3))
     2
     """
-    return r[0]
+    return r('n')
 
 
 def denom(r):
@@ -50,4 +80,4 @@ def denom(r):
     >>> denom(rational(2, 3))
     3
     """
-    return r[1]
+    return r('d')
